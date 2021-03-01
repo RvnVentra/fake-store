9a4490c011d2d3b2a32aba40d4323af2f76325e7
Ryan Feng <zhentfeng@gmail.com>
2021-03-01 15:45:12 -0500

    Can fetch data using querystring to display item

--- src/components/Items/ItemDetail/ItemDetail.js
+++ src/components/Items/ItemDetail/ItemDetail.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 export default function ItemDetail() {
-    let { id, test } = useParams();
+    let { id } = useParams();
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        Axios.get('https://fakestoreapi.com/products')
+            .then(res => {
+                const test = res.data;
+                const filtered = test.filter(item => item.id === parseInt(id));
+
+                setItem(filtered);
+            });
+    }, []);
+
+    const test = item[0];
+
     return (
         <div>
-            <h1>ID: {id}</h1>
+            <p>{test.title}</p>
+            <p>{test.description}</p>
+            <p>{test.price}</p>
         </div>
     );
 };
